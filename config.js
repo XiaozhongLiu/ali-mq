299d3ca3ed58688d718448d17e9b7ff0bac672b8
Xiaozhong <xiaozhong.liu@outlook.com>
2016-12-22 18:11:33 +0800

    use RAM sub account on client side which is only authorized to subscribe other than to send a message.

--- config.js
+++ config.js
@@ -1,13 +1,24 @@
 module.exports = {
 
+
+
     Port: 3000,
+
+
 
     /**
      * Ali MQ http conn config.
      */
     Host: 'http://publictest-rest.ons.aliyun.com/',
-    AccessKey: 'LTAIJOZNupLLSfMS',
-    SecretKey: 'Sva8mlOmohcqPaH8RbGQx2ZwhxZRi1',
+
+    // // 主账号
+    // AccessKey: 'LTAIJOZNupLLSfMS',
+    // SecretKey: 'Sva8mlOmohcqPaH8RbGQx2ZwhxZRi1',
+
+    // RAM子账号: recv_account
+    AccessKey: 'LTAIgKt7mpwnLWhr',
+    SecretKey: 'q9pNResj2kbrZ4HgB8H6vCJLALEWmI',
+
     Topic: 'topic_test_sole_1',
     PID: 'PID_test_sole_1',
     CIDArr: [
--- mqtt.js
+++ mqtt.js
@@ -3,9 +3,15 @@ let mqtt = require('mqtt');
 // 测试服
 let host = 'mqtt://mqtt-test.cn-qingdao.aliyuncs.com';
 let topic = 'topic_test_sole_1/sub_topic_1';
+let clientId = 'GID_test_sole_1@@@DID_test_1';
+
+// 测试服主账号
 let username = 'LTAIJOZNupLLSfMS';
 let password = 'z/9clHgQLoZR8LnFs+RdDpgiHVo=';
-let clientId = 'GID_test_sole_1@@@DID_test_1';
+
+// // 测试服RAM子账号(仅可收,不可发): recv_account
+// let username = 'LTAIgKt7mpwnLWhr';
+// let password = 'oNlw2bPVUZ4htU+E5ymrTjGLDZY=';
 
 let client = mqtt.connect(host, {
     username,
